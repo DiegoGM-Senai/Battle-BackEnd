b67ef141d0b192a3be6a213142542f47ee498f72
DiegoGM-Senai <94566652+DiegoGM-Senai@users.noreply.github.com>
2022-11-01 16:05:22 -0300

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize')
 const sequelize = new Sequelize(
     dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
         host: dbConfig.HOST,
-        port: dbConfig.process.env.PORT,
+        port: process.env.PORT,
         dialect: dbConfig.DIALECT,
         pool:{
             max: dbConfig.pool.max,
