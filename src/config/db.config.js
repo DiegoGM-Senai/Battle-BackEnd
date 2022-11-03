d37a7159a6fd28435c6216309c38f7ff5cdb2df3
DiegoGM-Senai <94566652+DiegoGM-Senai@users.noreply.github.com>
2022-11-03 17:21:00 -0300

    Update db.config.js

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "senai",
-    DB: "users_db",
+    HOST: "ec2-44-195-132-31.compute-1.amazonaws.com",
+    USER: "wrsktfcfvnsayt",
+    PASSWORD: "1a6162821eab9f746f6e2ca3f9201d6661ce4a8315fa128f643fe9f7c98d4a80",
+    DB: "d53rjnqr3e87nc",
     DIALECT : "postgres",
     pool: {
         max: 5,
