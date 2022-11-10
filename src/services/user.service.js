2d84054a3e0cb4bb18ef7e49c9fdf2e729b4e712
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-11-10 17:35:52 -0300

    Update user.service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -35,7 +35,7 @@ exports.create = async(username, icon, email, password, twitter, instagram, disc
 exports.update = async(username, icon, email, password, twitter, instagram, discord, twitch, biografia, status, corP, corS, favoritados, conquistas, imgFundo, imgFundoDois, dataCriacao) => {
     try{
         await User.update({
-            username: username, icon: icon, email: email, password: password, twitter: twitter, instagram: instagram, discord: discord, twitter: twitter, biografia: biografia, status: status,  corP: corP, corS: corS, favoritados: favoritados, conquistas: conquistas, imgFundo: imgFundo, imgFundoDois: imgFundoDois, dataCriacao: dataCriacao
+            username: username, icon: icon, email: email, password: password, twitter: twitter, instagram: instagram, discord: discord, twitch: twitch, biografia: biografia, status: status,  corP: corP, corS: corS, favoritados: favoritados, conquistas: conquistas, imgFundo: imgFundo, imgFundoDois: imgFundoDois, dataCriacao: dataCriacao
         }, {where: {id: id}})
 
     }catch(e){
