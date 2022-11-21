8a636d9b28f2d57c4383da8186b63d384f884cc8
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-11-21 14:13:00 -0300

    Update db.config.js

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "ec2-44-195-132-31.compute-1.amazonaws.com",
-    USER: "wrsktfcfvnsayt",
-    PASSWORD: "1a6162821eab9f746f6e2ca3f9201d6661ce4a8315fa128f643fe9f7c98d4a80",
-    DB: "d53rjnqr3e87nc",
+    HOST: "container-us-west-45.railway.app",
+    USER: "postgres",
+    PASSWORD: "x58NMXM6DZRn5k4mH5qm",
+    DB: "railway",
     DIALECT : "postgres",
     pool: {
         max: 5,
