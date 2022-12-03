3591a5d0d27f3c2f67ec36781d23f89c8ed373b7
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-12-03 17:12:42 -0300

    Update torneio.service.js

--- src/services/torneio.service.js
+++ src/services/torneio.service.js
@@ -4,7 +4,7 @@ const Torneio = db.torneio
 exports.findAll = async () => {
     try {
         const torneio = await Torneio.findAll({
-            attributes: ['id', "nome", 'logo', 'descricaoLonga', 'descricaoBreve', 'imgFundo',  'thumbnail', 'participantes', 'gameId', 'chave', 'quantiaParticipantes']
+            attributes: ['id', "nome", 'logo', 'descricaoLonga', 'descricaoBreve', 'imgFundo',  'thumbnail', 'participantes', 'gameId', 'chave', 'quantiaParticipantes', 'donoCriacao', 'admins']
         })
         return torneio
     } catch (e) {
@@ -15,7 +15,7 @@ exports.findAll = async () => {
 exports.findByPk = async (id) => {
     try {
         const torneio = await Torneio.findByPk(id, {
-            attributes: ['id', "nome", 'logo', 'descricaoLonga', 'descricaoBreve', 'imgFundo',  'thumbnail', 'participantes', 'gameId', 'chave', 'quantiaParticipantes']
+            attributes: ['id', "nome", 'logo', 'descricaoLonga', 'descricaoBreve', 'imgFundo',  'thumbnail', 'participantes', 'gameId', 'chave', 'quantiaParticipantes', 'donoCriacao', 'admins']
         })
         return torneio
     } catch (e) {
@@ -23,19 +23,19 @@ exports.findByPk = async (id) => {
     }
 }
 
-exports.create = async (nome, logo, descricaoLonga, descricaoBreve, imgFundo, thumbnail, participantes, gameId, chave, quantiaParticipantes) => {
+exports.create = async (nome, logo, descricaoLonga, descricaoBreve, imgFundo, thumbnail, participantes, gameId, chave, quantiaParticipantes, donoCriacao, admins) => {
     try {
-        const torneio = await Torneio.create({ nome: nome, logo: logo, imgFundo: imgFundo, descricaoLonga: descricaoLonga, descricaoBreve: descricaoBreve, thumbnail: thumbnail, participantes: participantes, gameId: gameId, chave: chave, quantiaParticipantes: quantiaParticipantes})
+        const torneio = await Torneio.create({ nome: nome, logo: logo, imgFundo: imgFundo, descricaoLonga: descricaoLonga, descricaoBreve: descricaoBreve, thumbnail: thumbnail, participantes: participantes, gameId: gameId, chave: chave, quantiaParticipantes: quantiaParticipantes, donoCriacao: donoCriacao, admins: admins})
         return torneio
     } catch (e) {
         return e.message
     }
 }
 
-exports.update = async (id ,nome, logo, descricaoLonga, descricaoBreve, imgFundo, thumbnail, participantes, gameId, chave, quantiaParticipantes) => {
+exports.update = async (id ,nome, logo, descricaoLonga, descricaoBreve, imgFundo, thumbnail, participantes, gameId, chave, quantiaParticipantes, donoCriacao, admins) => {
     try {
         await Torneio.update({
-            nome: nome, logo: logo, imgFundo: imgFundo, descricaoLonga: descricaoLonga, descricaoBreve: descricaoBreve, thumbnail: thumbnail, participantes: participantes, gameId: gameId, chave: chave, quantiaParticipantes: quantiaParticipantes
+            nome: nome, logo: logo, imgFundo: imgFundo, descricaoLonga: descricaoLonga, descricaoBreve: descricaoBreve, thumbnail: thumbnail, participantes: participantes, gameId: gameId, chave: chave, quantiaParticipantes: quantiaParticipantes, donoCriacao: donoCriacao, admins: admins
         }, { where: { id: id } })
 
     } catch (e) {
